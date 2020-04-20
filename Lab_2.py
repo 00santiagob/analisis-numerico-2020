cd23d7296693aa7272c9cb3759352a30cb9d4416
00sb <santiagobalog1998@gmail.com>
2020-04-20 11:56:50 -0300

    Lab_2.py en proceso

---
+++ Lab_2.py
@@ -0,0 +1,22 @@
+# Analisis Numerico (2020)
+# Trabajo de Laboratorio N°2
+# Autor: @00santiagob (GitHub)
+
+import numpy as np
+import math
+
+def rbisec(fun, I, err, mit):
+	# Algoritmo de Biseccion
+	# fun es una funcion que dado un x retorna f(x)
+	# I = [a,b] es un intervalo en los reales
+	# err es la tolerancia deseada del error
+	# mit es el numero maximo de iteraciones permitidas
+	
+	return hx, hf
+
+def ej1():
+	return rbisec()
+
+if __name__ == '__main__':
+	# ejx() ejecutara la funcion del ejercicio n° x
+	ej1()
