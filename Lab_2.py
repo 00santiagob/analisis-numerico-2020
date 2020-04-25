ea8b40939124963f48788409db3ec3a24ca4f972
00sb <santiagobalog1998@gmail.com>
2020-04-24 22:29:12 -0300

    lalala

--- Lab_2.py
+++ Lab_2.py
@@ -15,35 +15,47 @@ def rbisec(fun, I, err, mit):
 
     a = I[0]
     b = I[1]
+
     # Iniciamos las listas de puntos y evaluaciones
     hx = []
     hf = []
+    
     # Si el signo de a y b son distintos podemos comenzar
     # En vez de fun(a)*fun(b)<0 para calcular el signo usaremos sign()
     if sign(fun(a)) != sign(fun(b)):
         i = 0
-        e = 
-        while i < mit:
-            # r es el punto medio del intervalo
-            # Hay una forma mas convergente que r = (a + b) / 2
-            r = a + (b-a)/2
-            if fun(a)*fun(r) < 0:
-                b = r
-            elif fun(a)*fun(r) > 0:
-                a = r
+        # pm es el punto medio del intervalo
+        # Hay una forma mas convergente que pm = (a + b) / 2
+        pm = a + (b-a)/2
+        while i < mit and abs(fun(pm)) > err:
+        	i = i + 1
+            if fun(a)*fun(pm) < 0:
+                b = pm
+            elif fun(a)*fun(pm) > 0:
+                a = pm
             else:
-                # Entra si fun(r)=0
-                if abs(fun(r)) < err:
-                    hx.append(r)
-                    hf.append(fun(r))
+                # Entra si fun(pm)=0
+                if abs(fun(pm)) < err:
+                    hx.append(pm)
+                    hf.append(fun(pm))
                     break
-            hx.append(r)
-            hf.append(fun(r))
+            hx.append(pm)
+            hf.append(fun(pm))
+            pm = a + (b-a)/2
+    else:
+    	print("Elegir otro intervalo")
     return hx, hf
 
 
-def ej1():
-    return rbisec()
+def ej2_a():
+	a, b = map(int(),
+		input("Donde empiza y termina el intervalo: ").split(" "))
+	I = [a,b]
+	err = int(input("Tolerancia deseada del error: "))
+	mit = int(input("Numero maximo de iteraciones: "))
+    hx, hf = rbisec(fun,I,err,mit)
+    print("Historial de puntos medios:", hx,
+    	"\sHistorial de los respectivos puntos medios:", hf)
 
 
 if __name__ == '__main__':
