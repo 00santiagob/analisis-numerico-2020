d13bd7bd741882d58ebd24ad9f62650a5702c8c1
00santiagob <santiagobalog1998@gmail.com>
2020-04-28 15:10:55 -0300

    Correcciones del parcial

--- Parcial_1.py
+++ Parcial_1.py
@@ -9,19 +9,17 @@ from math import sqrt, exp
 def fibonacci():
     # Inicializo la lista donde se almacenara la sucesion de fibonacci
     fb = []
-    n = int(input("Ingresar el valor de n: "))
-    if n < 0:
+    n = input("Ingresar el valor de n: ")
+    if type(n) != int:
+        print("El valor de 'n' debe ser un entero positivo")
+    elif n < 0:
         print("El valor de 'n' no puede ser menor que 0")
-    elif n == 0:
-        fb.append(0)
-    elif n == 1:
-        fb.append(0)
-        fb.append(1)
     else:
-        fb.append(0)
-        fb.append(1)
         for i in range(2, n+1):
-            fb.append(fb[i-2] + fb[i-1])
+            if i <= 1:
+                fb.append(i)
+            else:
+                fb.append(fb[i-2] + fb[i-1])
     print(fb)
     return fb
 
