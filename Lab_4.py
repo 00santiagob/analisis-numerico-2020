988e09040b8709701c97a193be6d513dfeba0a15
00santiagob <santiagobalog1998@gmail.com>
2020-05-08 15:11:14 -0300

    otra vez lo mismo

--- Lab_4.py
+++ Lab_4.py
@@ -68,11 +68,61 @@ def ej1_b():
 
 
 def ej2_a():
-    pass
+    def f(x): return asin(x)
+    x = np.linspace(0, 1, 50)
+    y = []
+    for i in x:
+        y.append(f(i))
+    residuos = []
+    for n in range(6):
+        residuos.append(list(np.polyfit(x, y, n, full=True)))
+    print("Residuos para n={}:".format(n), *residuos, sep='\n')
+    sumTotal = 0
+    sumParcial = []
+    for i in residuos:
+        sumTotal = sumTotal + sum(i)
+        sumParcial.append(sum(i))
+    print("Suma Parcial:", *sumParcial, sep='\n')
+    print("Suma Total:", sumTotal)
 
 
 def ej2_b():
-    pass
+    def f(x): return cos(x)
+    x = np.linspace(0, 4*pi, 50)
+    y = []
+    for i in x:
+        y.append(f(i))
+    residuos = []
+    for n in range(6):
+        residuos.append(list(np.polyfit(x, y, n)))
+    print("Residuos para n={}:".format(n), *residuos, sep='\n')
+    sumTotal = 0
+    sumParcial = []
+    for i in residuos:
+        sumTotal = sumTotal + sum(i)
+        sumParcial.append(sum(i))
+    print("Suma Parcial:", *sumParcial, sep='\n')
+    print("Suma Total:", sumTotal)
+
+
+def ej3_a():
+    data = np.loadtxt('analisis-numerico-2020/datos/datos3a.dat')
+    # y = C * x**A --> ln(y) = ln(C) + A*ln(x)
+    # entonces y_hat = c_hat + A**x_hat, donde y_hat = ln(y), c_hat = ln(C)
+    x = data[0]
+    y = data[1]
+    x_hat = np.log(x)
+    y_hat = np.log(y)
+    coef = np.polyfit(x_hat, y_hat, 1)
+    print("coef =", coef)
+    C = np.exp(coef[0])
+    A = np.exp(coef[1])
+    print(C, A)
+    print(np.polyval([A, C], 1))
+    print(np.polyval([C, A], 1))
+    print(np.polyval([2, 3], 1))
+    print(np.polyval([3, 2], 1))
+
 
 
 if __name__ == "__main__":
